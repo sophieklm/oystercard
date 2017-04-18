35239e29ea5c97b9d995ee66d57e17a14e9abbfd
Sophie Mann <sklm@live.co.uk>
2017-04-18 18:11:51 +0100

    refactors tests

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,8 +3,11 @@ require 'oystercard'
 describe Oystercard do
 
   before(:each) do
-    @station = double("Trafalgar Square")
+    @station = double(:station)
   end
+
+  min_fare = Oystercard::MIN_FARE
+  max_balance = Oystercard::MAX_BALANCE
 
   describe 'balance' do
     it 'sets a default balance to 0' do
@@ -12,14 +15,11 @@ describe Oystercard do
     end
   end
 
-  describe 'top_up' do
-    it {is_expected.to respond_to(:top_up).with(1).argument}
+  describe '#top_up' do
     it 'increases balance by argument value' do
       expect{subject.top_up(1)}.to change{subject.balance}.by 1
     end
-
     it 'raises an error when top up limit exceeded' do
-      max_balance = Oystercard::MAX_BALANCE
       expect{subject.top_up(max_balance + 1)}.to raise_error "Maximum card balance £#{max_balance}"
     end
   end
@@ -31,7 +31,6 @@ describe Oystercard do
   end
 
   describe '#touch_in' do
-    min_fare = Oystercard::MIN_FARE
     it 'changes in_journey to true' do
       subject.top_up(min_fare)
       subject.touch_in(@station)
@@ -48,7 +47,6 @@ describe Oystercard do
   end
 
   describe '#touch_out' do
-    min_fare = Oystercard::MIN_FARE
     it 'changes in_journey to false' do
       subject.touch_out
       expect(subject.in_journey?).to be false
