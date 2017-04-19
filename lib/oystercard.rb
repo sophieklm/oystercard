c76c8758c69d58608ec386c6a40b4000de0db37f
Sophie Mann <sklm@live.co.uk>
2017-04-19 18:47:46 +0100

    adds journeys hash to update store entry & exit stations

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,15 +3,16 @@ class Oystercard
   MAX_BALANCE = 90
   MIN_FARE = 2
 
-  attr_reader :balance, :entry_station
+  attr_reader :balance, :entry_station, :journeys
 
   def initialize
     @balance = 0
     @entry_station
+    @journeys = {}
   end
 
   def in_journey?
-    @entry_station != nil 
+    @entry_station != nil
   end
 
   def top_up(value)
@@ -23,12 +24,15 @@ class Oystercard
     fail "Insufficient funds, you need to have the minimum amount (£#{MIN_FARE}) for a single journey." if @balance < MIN_FARE
     @in_journey = true
     @entry_station = station
+    @journeys[:entry_station] = station
   end
 
-  def touch_out
+  def touch_out(station)
     deduct(MIN_FARE)
     @in_journey = false
     @entry_station = nil
+    @exit_station = station
+    @journeys[:exit_station] = station
   end
 
   private
