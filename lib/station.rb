9b50d4ddbbf093ed9138465d17b9b9626d7fc614
Sophie Mann <sklm@live.co.uk>
2017-04-20 13:28:49 +0100

    adds failing station file load tests for collaboration

--- lib/station.rb
+++ lib/station.rb
@@ -1,6 +1,12 @@
+require 'csv'
+
 class Station
 
   attr_reader :name, :zone
+
+  def self.load_stations(file)
+    File.exists?(file)
+  end
 
   def initialize(hash)
     @name = hash[:name]
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -8,4 +8,22 @@ describe Station do
   it 'has a zone' do
     expect(subject.zone).to eq(1)
   end
+
+  describe '.load_stations' do
+    file = CSV.generate do |csv|
+      csv << ["Trafalgar Square", 1]
+      csv << ["Edgware", 3]
+    end
+
+    it 'checks existence of file' do
+      expect(File).to receive(:exists?).with("file")
+    end
+
+    xit 'loads a list of stations from csv' do
+      expect(File).to receive(:open).with("file", "r").and_return(file)
+      Station.load_stations(file)
+    end
+  end
+
+
 end
